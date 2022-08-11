0f0b18243ad47b67c44518ea13f8d073aa0e004e
Agrima10 <95522198+Agrima10@users.noreply.github.com>
2022-08-11 18:31:21 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
 export default function About() {
-    let aboutstyle={
-        color: 'white',
-    }
+  let aboutstyle = {
+    color: 'white',
+  }
   return (
     <div className='container text-center' style={aboutstyle}>
-        <h2>What is it?</h2>
+      <h2>About</h2>
+      <p>CodePro is an online code editor which can be used for various languages such as for compiler and debugger tool for C/C++ languages. It is world's first online IDE which gives debugging facility with embedded gdb debugger. This is a very handy webapp for coders who love coding in online IDE but face unexpected crashes and tricky bugs in their code. CodePro provides debugging power to such users to help them out.
+        So all in all, its a great online IDE powered with code editor, compiler and debugger.</p>
     </div>
   )
 }
